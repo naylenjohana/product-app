a0b8aab8348c81665975b35b690876df5e788eed
Johana <naylenjohana@gmail.com>
2017-10-22 15:46:46 -0500

    Se crea el crud de productos

--- src/providers/sqlite-service/sqlite-service.ts
+++ src/providers/sqlite-service/sqlite-service.ts
@@ -5,12 +5,7 @@ import {SQLite, SQLiteObject} from "@ionic-native/sqlite";
 import {Platform} from "ionic-angular";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
 
-/*
-  Generated class for the SqliteServiceProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class SqliteServiceProvider {
 
@@ -48,7 +43,8 @@ export class SqliteServiceProvider {
         quantity REAL,
         price REAL,
         latitude REAL,
-        longitude REAL
+        longitude REAL,
+        image TEXT
       );`,
 
       `CREATE TABLE IF NOT EXISTS user (
@@ -68,5 +64,88 @@ export class SqliteServiceProvider {
       .catch(err=>console.error("ERROR creating tables: ", err));
   
   }
+
+  getProducts() {
+    return this.isReady()
+      .then(() => {
+        return this.database.executeSql("SELECT * FROM products", [])
+          .then(data => {
+            let products = [];
+            for(let i=0; i<data.rows.length; i++){
+              products.push(data.rows.item(i));
+            }
+            return products;
+          })
+          .catch(err => console.error(err));
+      })
+      .catch(err=>console.error("not ready: ", err));
+  }
+
+  getProduct(id: number) {
+    return this.isReady()
+      .then(() => {
+        return this.database.executeSql(`SELECT * FROM products WHERE id = ${id}`, [])
+          .then(data => {
+            if(data.rows.length) return data.rows.item(0);
+            return null;
+          })
+          .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
+  }
+
   
+  
+  updateProduct(id:number, name: string,type:string,quantity:number,price:number, latitude:string
+    ,lenght:string,image:string) {
+      return this.isReady()
+        .then(() => {
+          return this.database.executeSql(`UPDATE products SET name=('${name}') WHERE id=('${id}');`, {})
+            .then(result => {
+              if(result.insertId) return this.getProduct(result.insertId);
+            })
+            .catch(err => console.error(err));
+        });
+    }
+
+
+    addProduct(name: string,type:string,quantity:number,price:number, latitude:string
+      ,lenght:string,image:string) {
+      return this.isReady()
+        .then(() => {
+          return this.database.executeSql(`INSERT INTO products(name,type,quantity,price,latitude,lenght,image)
+           VALUES ('${name}','${type}','${quantity}','${price}','${latitude}'
+           ,'${lenght}','${image}');`, {})
+            .then(result => {
+              if(result.insertId) return this.getProduct(result.insertId);
+            })
+            .catch(err => console.error(err));
+        });
+    }
+  
+
+  deleteProduct(id: number) {
+    return this.isReady()
+      .then(() => {
+        return this.database.executeSql(`DELETE FROM products WHERE id = ${id}`, [])
+      })
+      .catch(err => console.error(err));
+  }
+
+  
+
+  private isReady() {
+    return new Promise((resolve, reject) => {
+      if(this.dbReady.getValue()) {
+        resolve();
+      } else {
+        this.dbReady.subscribe(ready => {
+          if(ready) resolve();
+        });
+      }
+    })
+  }
+
 }
+  
+
